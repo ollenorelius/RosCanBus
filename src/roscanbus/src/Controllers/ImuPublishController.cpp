0fd34b4fa8a7315d3daecc2da87798cf153d4c11
Olle Norelius <norelius.olle@gmail.com>
2020-12-08 16:42:24 +0100

    Fixed timestamps and units on IMU

--- src/roscanbus/src/Controllers/ImuPublishController.cpp
+++ src/roscanbus/src/Controllers/ImuPublishController.cpp
@@ -10,29 +10,39 @@ ImuPublishController::ImuPublishController(std::map<ImuData, CanSignalModel*> si
     signals_(signals),    
     rosNodeModel_(rosNodeModel)
 {
-    signal_pub_ = rosNodeModel_->getNodeHandle()->advertise<sensor_msgs::Imu>("imu", 100);
+    signal_pub_ = rosNodeModel_->getNodeHandle()->advertise<sensor_msgs::Imu>("/imu", 100);
 
     auto callback = [this](){publish();};
 
     signals_[ImuData::ACC_X]->getValueChanged()->connect(callback);
+    
 }
 
 void ImuPublishController::publish() 
 {
     sensor_msgs::Imu imu_message;
+
+    imu_message.header.frame_id = "base_link";
+
+    imu_message.header.stamp = ros::Time::now();
+
     imu_message.orientation.x = 0;
     imu_message.orientation.y = 0;
     imu_message.orientation.z = 0;
 
     imu_message.orientation_covariance[0] = -1;
 
-    imu_message.angular_velocity.x = signals_[ImuData::GYRO_X]->getValue();
-    imu_message.angular_velocity.y = signals_[ImuData::GYRO_Y]->getValue();
-    imu_message.angular_velocity.z = signals_[ImuData::GYRO_Z]->getValue();
 
-    imu_message.linear_acceleration.x = signals_[ImuData::ACC_X]->getValue();
-    imu_message.linear_acceleration.y = signals_[ImuData::ACC_Y]->getValue();
-    imu_message.linear_acceleration.z = signals_[ImuData::ACC_Z]->getValue();
+    const float gyro_scaler = 1000*3.1415/180.0;
+    imu_message.angular_velocity.x = signals_[ImuData::GYRO_X]->getValue()/32768.0*gyro_scaler;
+    imu_message.angular_velocity.y = signals_[ImuData::GYRO_Y]->getValue()/32768.0*gyro_scaler;
+    imu_message.angular_velocity.z = signals_[ImuData::GYRO_Z]->getValue()/32768.0*gyro_scaler;
+
+    const float g = 9.82;
+    const float acc_scaler = 2*g;
+    imu_message.linear_acceleration.x = signals_[ImuData::ACC_X]->getValue()/32768.0*acc_scaler;
+    imu_message.linear_acceleration.y = signals_[ImuData::ACC_Y]->getValue()/32768.0*acc_scaler;
+    imu_message.linear_acceleration.z = signals_[ImuData::ACC_Z]->getValue()/32768.0*acc_scaler;
 
 
     
